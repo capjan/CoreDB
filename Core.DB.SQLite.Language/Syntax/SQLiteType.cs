cf97a07491646993da666075a1ff09c9f1a52c63
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-11-22 13:53:02 +0100

    Added SQLite Storage Class and Type Affinity Constants

---
+++ Core.DB.SQLite.Language/Syntax/SQLiteType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DB.SQLite.Language.Syntax
+{
+    public class SQLiteStorageClass
+    {
+        public const string Null    = "NULL";
+        public const string Integer = "INTEGER";
+        public const string Real    = "REAL";
+        public const string Text    = "TEXT";
+        public const string Blob    = "BLOB";
+    }
+
+    public class SQLiteTypeAffinity
+    {
+        public const string Text    = "TEXT";
+        public const string Numeric = "NUMERIC";
+        public const string Integer = "INTEGER";
+        public const string Real    = "REAL";
+        public const string Blob    = "BLOB";
+    }
+}
